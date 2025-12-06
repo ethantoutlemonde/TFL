a4885a01acc4ce778ae04134a19471ea75016bd0
guillaume-boop <guillaume.alameda@gmail.com>
2025-12-06 23:13:46 +0100

    ethan le vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  // base: '/TFL/',
-  base: '/TFL/frontend/',
+   base: '/TFL/',
+  //base: '/TFL/frontend/',
 
   plugins: [react()],
 
