e615e7bc29a1f6667ac5ee91472f746db16d87ef
ethanG <ethangunzb@gmail.com>
2025-12-06 16:02:58 +0100

    change

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/TFL/',
+  // base: '/TFL/',
+  base: '/TFL/frontend/',
 
   plugins: [react()],
 
